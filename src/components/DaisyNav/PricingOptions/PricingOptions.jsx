2e4790f2805fe2244a89df3b8ed12a2560d63366
tomafouzder <tomafouzder12@gmail.com>
2025-10-17 16:19:42 +0600

    added this update card styles

--- src/components/DaisyNav/PricingOptions/PricingOptions.jsx
+++ src/components/DaisyNav/PricingOptions/PricingOptions.jsx
@@ -1,21 +1,29 @@
-import React, {use} from 'react';
+import React, { use } from 'react';
 import PricingCard from '../../PricingCard/PricingCard';
+import img1 from '../../../assets/pexels-anush-1431282.jpg'
 
-const PricingOptions = ({pricingPromise}) => {
+const PricingOptions = ({ pricingPromise }) => {
 
     const pricingData = use(pricingPromise);
 
     // console.log(pricingData);
-    
+
     return (
-        <div className='w-11/12 mx-auto'>
-            <h2 className='text-5xl gb-base-100 p-4  border-2 border-white  font-bold text-center mt-50 mb-10'>Get Our Membership</h2>
+        <div className='w-11/12 mx-auto pt-10 '
+
+        >
+            <div className='md:hidden flex justify-end h-[300px]'>
+                <img  src={img1} alt="" />
+            </div>
+            <div className='md:mt-10 mt-2 mb-10'>
+                <h2 className='text-5xl gb-base-100 p-4  border-2 border-white  font-bold text-center pb-10 pt-10'>Get Our Membership</h2>
+            </div>
 
             <div className='grid md:grid-cols-3 gap-8'>
                 {
-                    pricingData.map(pricing => <PricingCard 
-                    pricing={pricing}
-                    key={pricing.id}
+                    pricingData.map(pricing => <PricingCard
+                        pricing={pricing}
+                        key={pricing.id}
                     ></PricingCard>)
                 }
             </div>
--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CircleCheckBig } from 'lucide-react';
+import { CalendarClock } from 'lucide-react';
 import PricingFeature from './PricingFeature';
 
 const PricingCard = ({ pricing }) => {
@@ -10,12 +10,12 @@ const PricingCard = ({ pricing }) => {
             <div>
                 <h1 className="text-7xl mb-2 font-bold">{name}</h1>
                 <h4 className="text-4xl mb-2 font-medium">{price}</h4>
-                <h3 className="text-2xl mb-2 font-medium flex  items-center"><CircleCheckBig className='mr-2'></CircleCheckBig> {duration}</h3>
+                <h3 className="text-2xl mb-2 font-medium flex  items-center"><CalendarClock className='mr-2'></CalendarClock> {duration}</h3>
             </div>
             {/* card body */}
             <div>
                 <p className='bg-gradient-to-r from-amber-600 via-amber-400 to-amber-600 text-gray-950 font-medium p-2 rounded-box shadow mt-4'>
-                 <span className='text-xl font-bold '>description: <br /></span>   {pricing.description}</p>
+                 <span className='text-xl font-bold'>description: <br /></span>   {pricing.description}</p>
 
                 <div className='p-4 mt-8 rounded-box bg-gradient-to-r from-amber-700 to-amber-400 shadow-2xl'>
                     <p  className='text-xl font-bold mt-2 mb-2'>Features:</p>
